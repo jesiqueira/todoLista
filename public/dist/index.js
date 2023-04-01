3ab0e9723b19ff09a2966f2077df33df7a3bdc9e
jose edmar de siqueira <edmar.ade@gmail.com>
2023-04-01 08:43:07 -0300

    criado ts para todo e reminders

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,26 +1,39 @@
 "use strict";
+;
 (() => {
     const todo = {
-        description: "todo",
+        description: 'todo',
         done: false,
     };
     const remender = {
-        description: "reminder",
-        date: "15.12.2021",
+        description: 'reminder',
+        date: '15.12.2021',
     };
     const taskView = {
         render(tasks) {
-            const tasksList = document.querySelector("#tasksList");
+            // selecionar lista no html
+            const tasksList = document.querySelector('#tasksList');
+            // Limpar a lista
             while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
             }
+            // add itens da lista
             tasks.forEach((tasks) => {
-                const li = document.createElement("li");
+                const li = document.createElement('li');
                 const textNode = document.createTextNode(JSON.stringify(tasks));
                 li.appendChild(textNode);
                 tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
             });
-            const TaskController = () => { };
         },
     };
+    const TaskController = (view) => {
+        var _a;
+        const tasks = [todo, remender];
+        const handleEvent = (event) => {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document.querySelector('#taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+    };
+    TaskController(taskView);
 })();
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,42 @@
-(() => {
+;(() => {
   const todo = {
-    description: "todo",
+    description: 'todo',
     done: false,
-  };
+  }
 
   const remender = {
-    description: "reminder",
-    date: "15.12.2021",
-  };
+    description: 'reminder',
+    date: '15.12.2021',
+  }
 
   const taskView = {
     render(tasks: Array<Object>) {
-      const tasksList = document.querySelector("#tasksList");
+      // selecionar lista no html
+      const tasksList = document.querySelector('#tasksList')
 
+      // Limpar a lista
       while (tasksList?.firstChild) {
-        tasksList.removeChild(tasksList.firstChild);
+        tasksList.removeChild(tasksList.firstChild)
       }
 
+      // add itens da lista
       tasks.forEach((tasks) => {
-        const li = document.createElement("li");
-        const textNode = document.createTextNode(JSON.stringify(tasks));
-        li.appendChild(textNode);
-        tasksList?.appendChild(li);
-      });
-
-      const TaskController = () => {};
+        const li = document.createElement('li')
+        const textNode = document.createTextNode(JSON.stringify(tasks))
+        li.appendChild(textNode)
+        tasksList?.appendChild(li)
+      })
     },
-  };
-})();
+  }
+  const TaskController = (view: typeof taskView) => {
+    const tasks: Array<Object> = [todo, remender]
+
+    const handleEvent = (event: Event) => {
+      event.preventDefault()
+      view.render(tasks)
+    }
+
+    document.querySelector('#taskForm')?.addEventListener('submit', handleEvent)
+  }
+  TaskController(taskView)
+})()
